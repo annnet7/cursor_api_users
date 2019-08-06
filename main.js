2e7acaf303dd522fb35e10478bccf72e2721d160
annnet7 <51447220+annnet7@users.noreply.github.com>
2019-08-06 16:00:41 +0300

    new css user add, delete and edit functions

--- main.js
+++ main.js
@@ -6,17 +6,102 @@
     });
     //get all items
     const usersRespons = await usersApi.get("/users");
-    console.log(usersRespons.data.data);
 
-    //remove item by id
-    const res = await usersApi.delete("users/" + "5d42d6745d05110014015992");
-    //add new item
-    usersApi.post('/users', { name: 'new_old_user', age: 100 })
-        .then(function(response) {
-            console.log(response);
+    let usersList = usersRespons.data.data;
+    //console.log(usersList);
+
+    function addNewRowInTable(item) {
+        //console.log(item);
+        var rowDiw = document.createElement('div');
+        rowDiw.className = 'row';
+        document.getElementById('userList').appendChild(rowDiw);
+
+        let nameDiv = document.createElement('input');
+        nameDiv.className = 'userInput';
+        nameDiv.value = item['name'];
+        rowDiw.appendChild(nameDiv);
+
+        let ageDiv = document.createElement('input');
+        ageDiv.className = 'userInput';
+        ageDiv.value = item['age'];
+        ageDiv.type = "number";
+        rowDiw.appendChild(ageDiv);
+
+
+        //del button
+        let delButton = document.createElement('button');
+        delButton.metaId = item['id'];
+        delButton.className = 'dellBtn';
+        delButton.innerText = "Delete";
+        rowDiw.appendChild(delButton);
+        //save button
+        let saveButton = document.createElement('button');
+        saveButton.metaId = item['id'];
+        saveButton.className = 'saveBtn';
+        saveButton.innerText = "Save";
+        rowDiw.appendChild(saveButton);
+
+    }
+
+    usersList.forEach(user => {
+        addNewRowInTable(user);
+    });
+
+    //события на кнопку для удаления
+    let delBtnList = [...document.getElementsByClassName('dellBtn')];
+    // console.log(delBtnList);
+    delBtnList.forEach(delButton => {
+        delButton.addEventListener('click', async function() {
+            console.log(this.metaId);
+            //удаляю дивку
+            //console.log(delButton.parentElement.parentElement);
+            delButton.parentElement.remove();
+            //удаляю строку на сервере
+            const res = await usersApi.delete("users/" + this.metaId);
         })
-        .catch(function(error) {
-            console.log(error);
+    });
+    //события на кнопку для изменений
+    let saveBtnList = [...document.getElementsByClassName('saveBtn')];
+    // console.log(delBtnList);
+    saveBtnList.forEach(saveButton => {
+        saveButton.addEventListener('click', function() {
+            let changeName = this.parentElement.childNodes[0].value;
+            let changeAge = +this.parentElement.childNodes[1].value;
+            // let userAge = +document.getElementById('age').value;
+            console.log(changeName, changeAge);
+            usersApi.put('/users/' + saveButton.metaId, { name: changeName, age: changeAge })
+                .then(function(response) {
+                    console.log(response);
+
+                })
+                .catch(function(error) {
+                    console.log(error);
+                })
         })
+    });
+
+    //console.log(document.getElementById('name').value);
+    let regUserButton = document.getElementById('regUserBtn');
+    regUserButton.addEventListener('click', function() {
+            let userName = document.getElementById('name').value;
+            let userAge = +document.getElementById('age').value;
+            console.log(userName, userAge);
+            usersApi.post('/users', { name: userName, age: userAge })
+                .then(function(response) {
+                    console.log(response);
+                    addNewRowInTable(response.data.data);
+                })
+                .catch(function(error) {
+                    console.log(error);
+                })
+        })
+        //add new item
+        /* usersApi.post('/users', { name: 'new_old_user', age: 100 })
+             .then(function(response) {
+                 console.log(response);
+             })
+             .catch(function(error) {
+                 console.log(error);
+             })*/
 
 })();
