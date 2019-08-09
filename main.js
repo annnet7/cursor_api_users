93aab631c1e98e8558194028463136356fc9a76f
annnet7 <51447220+annnet7@users.noreply.github.com>
2019-08-09 12:59:21 +0300

    total class changes

--- main.js
+++ main.js
@@ -1,108 +1,110 @@
 (async() => {
 
-    const USERS_API = "https://test-users-api.herokuapp.com/";
+    function showUserCard(obj) {
+        let userCardDiv = document.createElement('div');
+        userCardDiv.className = "row";
+        document.getElementById('userList').appendChild(userCardDiv);
+
+        let userName = document.createElement('input');
+        userName.value = obj.name;
+        userName.className = "userInput";
+        let userAge = document.createElement('input');
+        userAge.type = "number";
+        userAge.value = obj.age;
+        userAge.className = "userInput";
+        let deleteBtn = document.createElement('button');
+        deleteBtn.innerText = "Dellete";
+        deleteBtn.className = 'dellBtn';
+        //response от создания возвращает _id, а get просто id
+        if (obj._id) obj.id = obj._id;
+        ////
+        deleteBtn.userId = obj.id;
+        deleteBtn.addEventListener('click', function() {
+            console.log(deleteBtn.userId);
+            DeleteUser(deleteBtn.userId);
+            deleteBtn.parentElement.remove();
+        });
+
+        let saveBtn = document.createElement('button');
+        saveBtn.innerText = "Save";
+        saveBtn.className = 'saveBtn';
+        saveBtn.userId = obj.id;
+        saveBtn.addEventListener('click', function() {
+
+            let name = saveBtn.parentElement.childNodes[0].value;
+            let age = saveBtn.parentElement.childNodes[1].value;
+            let userId = saveBtn.parentElement.childNodes[3].userId;
+            let newUserObj = {
+                id: userId,
+                name: name,
+                age: age
+            };
+            EditUser(newUserObj);
+        });
+
+        userCardDiv.appendChild(userName);
+        userCardDiv.appendChild(userAge);
+        userCardDiv.appendChild(deleteBtn);
+        userCardDiv.appendChild(saveBtn);
+    }
+
+    const apiLinc = 'https://test-users-api.herokuapp.com';
     const usersApi = axios.create({
-        baseURL: USERS_API
+        baseURL: apiLinc
     });
-    //get all items
-    const usersRespons = await usersApi.get("/users");
 
-    let usersList = usersRespons.data.data;
-    //console.log(usersList);
-
-    function addNewRowInTable(item) {
-        //console.log(item);
-        var rowDiw = document.createElement('div');
-        rowDiw.className = 'row';
-        document.getElementById('userList').appendChild(rowDiw);
-
-        let nameDiv = document.createElement('input');
-        nameDiv.className = 'userInput';
-        nameDiv.value = item['name'];
-        rowDiw.appendChild(nameDiv);
-
-        let ageDiv = document.createElement('input');
-        ageDiv.className = 'userInput';
-        ageDiv.value = item['age'];
-        ageDiv.type = "number";
-        rowDiw.appendChild(ageDiv);
-
-
-        //del button
-        let delButton = document.createElement('button');
-        if (item._id) item.id = item._id;
-        delButton.metaId = item.id;
-        delButton.className = 'dellBtn';
-        delButton.innerText = "Delete";
-        rowDiw.appendChild(delButton);
-        //save button
-        let saveButton = document.createElement('button');
-        saveButton.metaId = item['id'];
-        saveButton.className = 'saveBtn';
-        saveButton.innerText = "Save";
-        rowDiw.appendChild(saveButton);
+    async function GetUsers() {
+        let res = await usersApi.get('/users');
+        let data = res.data.data;
+        return data;
+    }
+    async function CreateNewUser(newUser) {
+        let res = await usersApi.post('/users', newUser)
+            .then(function(response) {
+                console.log(response);
+                showUserCard(response.data.data);
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
 
     }
 
-    usersList.forEach(user => {
-        addNewRowInTable(user);
-    });
+    async function DeleteUser(id) {
+        let res = await usersApi.delete('/users/' + id)
+            .then(function(response) {
+                console.log(response);
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+    }
+    async function EditUser(obj) {
+        let res = await usersApi.put('/users/' + obj.id, { name: obj.name, age: obj.age })
+            .then(function(response) {
+                console.log(response);
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+    }
 
-    //события на кнопку для удаления
-    let delBtnList = [...document.getElementsByClassName('dellBtn')];
-    // console.log(delBtnList);
-    delBtnList.forEach(delButton => {
-        delButton.addEventListener('click', async function() {
-            console.log(this.metaId);
-            //удаляю дивку
-            //console.log(delButton.parentElement.parentElement);
-            delButton.parentElement.remove();
-            //удаляю строку на сервере
-            const res = await usersApi.delete("users/" + this.metaId);
-        })
-    });
-    //события на кнопку для изменений
-    let saveBtnList = [...document.getElementsByClassName('saveBtn')];
-    // console.log(delBtnList);
-    saveBtnList.forEach(saveButton => {
-        saveButton.addEventListener('click', function() {
-            let changeName = this.parentElement.childNodes[0].value;
-            let changeAge = +this.parentElement.childNodes[1].value;
-            // let userAge = +document.getElementById('age').value;
-            console.log(changeName, changeAge);
-            usersApi.put('/users/' + saveButton.metaId, { name: changeName, age: changeAge })
-                .then(function(response) {
-                    console.log(response);
 
-                })
-                .catch(function(error) {
-                    console.log(error);
-                })
-        })
+    //first read
+    let usersArray = await GetUsers();
+    usersArray.forEach(element => {
+        showUserCard(element);
     });
-
-    //console.log(document.getElementById('name').value);
-    let regUserButton = document.getElementById('regUserBtn');
-    regUserButton.addEventListener('click', function() {
-            let userName = document.getElementById('name').value;
-            let userAge = +document.getElementById('age').value;
-            console.log(userName, userAge);
-            usersApi.post('/users', { name: userName, age: userAge })
-                .then(function(response) {
-                    console.log(response);
-                    addNewRowInTable(response.data.data);
-                })
-                .catch(function(error) {
-                    console.log(error);
-                })
-        })
-        //add new item
-        /* usersApi.post('/users', { name: 'new_old_user', age: 100 })
-             .then(function(response) {
-                 console.log(response);
-             })
-             .catch(function(error) {
-                 console.log(error);
-             })*/
+    //create new user
+    let createBtn = document.getElementById('addNewUser');
+    createBtn.addEventListener('click', function() {
+        let name = document.getElementById('nameInput').value;
+        let age = document.getElementById('ageInput').value;
+        let newUserObj = {
+            name: name,
+            age: age
+        };
+        CreateNewUser(newUserObj);
+    })
 
 })();
